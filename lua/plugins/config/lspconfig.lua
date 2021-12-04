9577212cd1961c8157831017301a2306aa32d942
Neel Basak <neelfrost@gmail.com>
2021-12-04 20:18:06 +0530

    refactor(lspconfig): improve setup_ls pattern

--- lua/plugins/config/lspconfig.lua
+++ lua/plugins/config/lspconfig.lua
@@ -49,60 +49,70 @@ local on_attach = function(client, bufnr)
 end
 
 -- Setup language servers
+-- https://www.reddit.com/r/neovim/comments/r6gouy/migrating_from_nvim_051_to_nvim_060/hmvzsps/?context=3
 function M.setup_ls(lspconfig, capabilities)
     -- Language servers to be setup:
     local servers = { "pyright", "sumneko_lua", "omnisharp", "null-ls" }
 
-    for _, lsp in ipairs(servers) do
-        if lsp == "sumneko_lua" then
-            local sumneko_root_path = "C:\\tools\\lua-language-server"
-            local sumneko_binary = "C:\\tools\\lua-language-server\\bin\\lua-language-server.exe"
-            local runtime_path = vim.split(package.path, ";")
-            table.insert(runtime_path, "lua/?.lua")
-            table.insert(runtime_path, "lua/?/init.lua")
-            lspconfig[lsp].setup({
-                on_attach = on_attach,
-                capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities),
-                cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = "LuaJIT",
-                            path = runtime_path,
-                        },
-                        diagnostics = {
-                            globals = { "vim", "use" },
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                            maxPreload = 10000,
-                            preloadFileSize = 10000,
-                            checkThirdParty = false,
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
+    -- Sumneko_lua vars
+    local sumneko_root_path = "C:\\tools\\lua-language-server"
+    local sumneko_binary = "C:\\tools\\lua-language-server\\bin\\lua-language-server.exe"
+    local runtime_path = vim.split(package.path, ";")
+    table.insert(runtime_path, "lua/?.lua")
+    table.insert(runtime_path, "lua/?/init.lua")
+
+    -- Omnisharp vars
+    local pid = vim.fn.getpid()
+    local omnisharp_bin = vim.fn.trim(vim.fn.system("which omnisharp"))
+
+    -- Specific server config
+    local specific_configs = {
+        sumneko_lua = {
+            cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                        path = runtime_path,
+                    },
+                    diagnostics = {
+                        globals = { "vim", "use" },
+                    },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        maxPreload = 10000,
+                        preloadFileSize = 10000,
+                        checkThirdParty = false,
+                    },
+                    telemetry = {
+                        enable = false,
                     },
                 },
-            })
-        elseif lsp == "omnisharp" then
-            local pid = vim.fn.getpid()
-            local omnisharp_bin = vim.fn.trim(vim.fn.system("which omnisharp"))
-            lspconfig[lsp].setup({
-                on_attach = on_attach,
-                capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities),
-                cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
-                root_dir = function(fname)
-                    local dir = util.root_pattern(".csproj", ".sln")(fname) or util.path.dirname(fname)
-                    return vim.fn.has("win32") == 1 and string.format("'%s'", dir:lower()) or dir
-                end,
-            })
-        else
-            lspconfig[lsp].setup({
-                on_attach = on_attach,
-                capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities),
-            })
-        end
+            },
+        },
+        omnisharp = {
+            cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
+            root_dir = function(fname)
+                local dir = util.root_pattern(".csproj", ".sln")(fname) or util.path.dirname(fname)
+                return vim.fn.has("win32") == 1 and string.format("'%s'", dir:lower()) or dir
+            end,
+        },
+    }
+
+    -- Common config for all servers
+    local default_config = {
+        on_attach = on_attach,
+        capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities),
+        flags = {
+            debounce_text_changes = 500,
+        },
+    }
+
+    -- Setup ls
+    for _, lsp in ipairs(servers) do
+        local config = specific_configs[lsp] or {}
+        config = vim.tbl_extend("keep", config, default_config)
+        lspconfig[lsp].setup(config)
     end
 end
 
