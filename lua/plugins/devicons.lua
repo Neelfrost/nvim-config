6f094030d7dea67ea9d1af2d0e14ec56647de1a5
Neel Basak <neelfrost@gmail.com>
2021-12-21 22:34:54 +0530

    feat(devicons): add web-dev icons

--- lua/plugins/devicons.lua
+++ lua/plugins/devicons.lua
@@ -1,35 +1,60 @@
 require("nvim-web-devicons").setup({
-	override = {
-		tex = {
-			icon = "",
-			color = "#008080",
-			name = "Tex",
-		},
-		cls = {
-			icon = "",
-			color = "#008080",
-			name = "Cls",
-		},
-		sty = {
-			icon = "",
-			color = "#008080",
-			name = "Sty",
-		},
-		snippets = {
-			icon = "",
-			color = "#008080",
-			name = "Snippets",
-		},
-		tags = {
-			icon = "",
-			color = "#008080",
-			name = "Tags",
-		},
-		ps1 = {
-			icon = "ﲵ",
-			color = "#4478D2",
-			name = "Powershell",
-		},
-	},
-	default = true,
+    override = {
+        tex = {
+            icon = "",
+            color = "#008080",
+            name = "tex",
+        },
+        cls = {
+            icon = "",
+            color = "#008080",
+            name = "cls",
+        },
+        sty = {
+            icon = "",
+            color = "#008080",
+            name = "sty",
+        },
+        snippets = {
+            icon = "",
+            color = "#a9b665",
+            name = "snippets",
+        },
+        tags = {
+            icon = "",
+            color = "#6d8086",
+            name = "tags",
+        },
+        prettierrc = {
+            icon = "",
+            color = "#6d8086",
+            name = "prettierrc",
+        },
+        ps1 = {
+            icon = "ﲵ",
+            color = "#4478d2",
+            name = "ps1",
+        },
+        css = {
+            icon = "",
+            color = "#28a9e0",
+            name = "css",
+        },
+        html = {
+            icon = "",
+            color = "#f1662b",
+            name = "html",
+        },
+        js = {
+            icon = "",
+            color = "#e5c61d",
+            name = "js",
+        },
+        xml = {
+            icon = "",
+            color = "#f1662b",
+            name = "xml",
+        },
+    },
+    default = true,
 })
