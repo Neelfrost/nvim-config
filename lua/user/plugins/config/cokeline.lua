f600c9984f69e4c8ecaaa90c5bf415bb4fd23e4a
Neel Basak <neelfrost@gmail.com>
2022-03-10 23:13:52 +0530

    fix(cokeline): update config to reflect changes in release

--- lua/user/plugins/config/cokeline.lua
+++ lua/user/plugins/config/cokeline.lua
@@ -4,14 +4,6 @@ local is_picking_close = require("cokeline.mappings").is_picking_close
 
 local buffer_ignore_types = { "terminal", "quickfix" }
 
---- Create a padding component
---- @param n string amount of padding (default 1)
---- @return table component
-local function padding(n)
-    n = n or 1
-    return { text = string.rep(" ", n) }
-end
-
 local colors = require("themer.modules.core.api").get_cp(SCHEME)
 local active_fg = colors.blue
 local active_bg = get_hex("Normal", "bg")
@@ -20,11 +12,18 @@ local inactive_bg = get_hex("TabLineFill", "bg")
 local modified_fg = colors.red
 local switch_fg = colors.green
 
+--- Create a padding component
+--- @param n string amount of padding (default 1)
+--- @return table component
+local function padding(n)
+    return { text = string.rep(" ", n or 1) }
+end
+
 require("cokeline").setup({
     show_if_buffers_are_at_least = 1,
 
     buffers = {
-        filter_valid = function(buffer)
+        filter_visible = function(buffer)
             return not vim.tbl_contains(buffer_ignore_types, buffer.type)
         end,
         new_buffers_position = "next",
@@ -36,32 +35,30 @@ require("cokeline").setup({
 
     rendering = {
         max_buffer_width = 24,
-        left_sidebar = {
-            filetype = "NvimTree",
-            components = {
-                {
-                    text = "  NvimTree",
-                    hl = {
-                        fg = active_fg,
-                        bg = active_bg,
-                        style = "bold",
-                    },
+    },
+
+    sidebar = {
+        filetype = "neo-tree",
+        components = {
+            {
+                text = "  Neo-Tree",
+                hl = {
+                    fg = active_fg,
+                    bg = active_bg,
+                    style = "bold",
                 },
             },
         },
     },
 
     default_hl = {
-        focused = {
-            fg = active_fg,
-            bg = active_bg,
-            style = "bold",
-        },
-        unfocused = {
-            fg = inactive_fg,
-            bg = inactive_bg,
-            style = "bold",
-        },
+        fg = function(buffer)
+            return buffer.is_focused and active_fg or inactive_fg
+        end,
+        bg = function(buffer)
+            return buffer.is_focused and active_bg or inactive_bg
+        end,
+        style = "bold",
     },
 
     components = {
@@ -71,15 +68,9 @@ require("cokeline").setup({
                 return (is_picking_focus() or is_picking_close()) and (buffer.pick_letter .. ":")
                     or (buffer.index .. ":")
             end,
-            hl = {
-                fg = function()
-                    if is_picking_close() then
-                        return modified_fg
-                    elseif is_picking_focus() then
-                        return switch_fg
-                    end
-                end,
-            },
+            fg = function()
+                return is_picking_close() and modified_fg or (is_picking_focus() and switch_fg or nil)
+            end,
         },
         padding(),
         {
@@ -102,13 +93,9 @@ require("cokeline").setup({
                 return buffer.is_modified and "" or ""
             end,
             delete_buffer_on_left_click = true,
-            hl = {
-                fg = function(buffer)
-                    if buffer.is_modified then
-                        return modified_fg
-                    end
-                end,
-            },
+            fg = function(buffer)
+                return buffer.is_modified and modified_fg or nil
+            end,
         },
         padding(2),
     },
