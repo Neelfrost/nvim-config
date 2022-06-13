38ad840751deba723fb5d251bcdc604cc71f7539
Neel Basak <neelfrost@gmail.com>
2022-06-13 10:25:21 +0530

    feat(themer): update highlights

--- lua/user/plugins/config/themer/init.lua
+++ lua/user/plugins/config/themer/init.lua
@@ -1,35 +1,33 @@
 local colors = require("themer.modules.core.api").get_cp(SCHEME)
-local lighten = require("themer.utils.colors").lighten
+local utils = require("user.plugins.config.themer.utils")
 local darken = require("themer.utils.colors").darken
 
-colors.bg.lighter = lighten(colors.bg.base, 0.9, "#4C4C4C")
+colors.bg.lighter = utils.adjust_color(colors.bg.base, 5)
 colors.bg.darker = darken(colors.bg.base, 0.9, "#000000")
 
 require("themer").setup({
     colorscheme = SCHEME,
     styles = {
         comment = { style = "italic" },
-        ["function"] = { style = "italic" },
+        ["function"] = { style = "bold" },
     },
     remaps = {
         highlights = {
             globals = {
                 -- themer
                 themer = {
-                    ThemerNormalFloat = { bg = colors.bg.darker },
                     ThemerBorder = { fg = colors.bg.darker, bg = colors.bg.darker },
+                    ThemerNormalFloat = { bg = colors.bg.darker },
+                    ThemerSelected = { bg = darken(colors.bg.base, 0.8, colors.magenta) },
                 },
                 -- base
                 base = {
-                    Folded = {
-                        fg = colors.syntax.comment or colors.dimmed.subtle,
-                        bg = lighten(colors.bg.base, 0.9, colors.syntax.comment),
-                    },
+                    Folded = { fg = utils.adjust_color(colors.fg, -80), bg = colors.bg.lighter },
                     FoldColumn = { fg = colors.blue, bg = colors.bg.base },
                     LineNr = { fg = colors.blue, bg = colors.bg.base },
                     LineNrAbove = { link = "ThemerDimmed" },
                     LineNrBelow = { link = "ThemerDimmed" },
-                    MatchParen = { fg = colors.diagnostic.warn, bg = "None", style = "bold" },
+                    MatchParen = { fg = colors.diagnostic.warn, bg = "None", style = "underline" },
                     TabLineFill = { fg = colors.bg.lighter, bg = colors.bg.lighter },
                     SpellBad = { fg = "#ee6d85", bg = "black", style = "bold" },
                     SpellCap = { fg = colors.green, bg = "black", style = "bold" },
@@ -38,33 +36,25 @@ require("themer").setup({
                     VertSplit = { fg = colors.bg.lighter, bg = "None", style = "None" },
                     StatusLine = { link = "VertSplit", style = "None" },
                     StatusLineNC = { link = "VertSplit", style = "None" },
-                    NormalFloat = { link = "ThemerNormalFloat" },
+                    NormalFloat = { bg = colors.bg.darker },
                     FloatBorder = { link = "ThemerBorder" },
                     PmenuSel = { bg = colors.syntax.comment or colors.dimmed.subtle },
                 },
                 plugins = {
                     virtcolumn = {
-                        -- virtcolumn
-                        VirtColumn = {
-                            fg = darken(colors.blue, 0.5, "#000000"),
-                            bg = "None",
-                        },
+                        VirtColumn = { fg = utils.adjust_color(colors.red, -50), bg = "None" },
                     },
                     indentline = {
-                        -- indentline
-                        IndentBlanklineChar = { fg = lighten(colors.bg.base, 0.9, "#FFFFFF") },
+                        IndentBlanklineChar = { fg = utils.adjust_color(colors.bg.base, 20) },
                     },
                     cmp = {
-                        -- cmp
                         CmpItemMenu = { fg = colors.syntax.comment or colors.dimmed.subtle, bg = colors.pum.bg },
                         CmpDocumentation = { fg = colors.pum.fg, bg = colors.pum.bg },
                         CmpDocumentationBorder = { bg = colors.pum.bg },
-
                         CmpItemAbbr = { fg = colors.fg, style = "NONE" },
                         CmpItemAbbrDeprecated = { fg = colors.fg },
                         CmpItemAbbrMatch = { fg = colors.green, style = "bold" },
                         CmpItemAbbrMatchFuzzy = { fg = colors.blue },
-
                         CmpItemKindText = { fg = colors.orange },
                         CmpItemKindMethod = { fg = colors.blue },
                         CmpItemKindFunction = { fg = colors.blue },
@@ -90,7 +80,6 @@ require("themer").setup({
                         TelescopeNormal = { link = "ThemerNormalFloat" },
                     },
                     vimtex = {
-                        -- vimtex
                         texStatement = { link = "ThemerField" },
                         texOnlyMath = { link = "ThemerPunctuation" },
                         texDefName = { link = "ThemerType" },
@@ -115,6 +104,12 @@ require("themer").setup({
                         texMathEnvArgName = { link = "ThemerConstantBuiltIn" },
                         texTitleArg = { link = "ThemerConstantBuiltIn" },
                         texAuthorArg = { link = "ThemerConstantBuiltIn" },
+                    },
+                    lsp = {
+                        DiagnosticUnderlineHint = { gui = "underline" },
+                        DiagnosticUnderlineInfo = { gui = "underline" },
+                        DiagnosticUnderlineWarn = { gui = "underline" },
+                        DiagnosticUnderlineError = { gui = "underline" },
                     },
                     neotree = {
                         NeoTreeRootName = { link = "ThemerMatch" },
---
+++ lua/user/plugins/config/themer/utils.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.adjust_color = function(col, amt)
+    local function clamp(component)
+        return math.min(math.max(component, 0), 255)
+    end
+
+    local num = tonumber(col:gsub("#", ""), 16)
+    local r = math.floor(num / 0x10000) + amt
+    local g = (math.floor(num / 0x100) % 0x100) + amt
+    local b = (num % 0x100) + amt
+    return string.format("#%02X", clamp(r) * 0x10000 + clamp(g) * 0x100 + clamp(b))
+end
+
+return M
