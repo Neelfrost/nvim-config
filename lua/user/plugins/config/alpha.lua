5558217b800f79a7141d00b83a6445d0f2b51394
Neel Basak <neelfrost@gmail.com>
2022-06-21 21:53:23 +0530

    feat(alpha): dynamic padding

--- lua/user/plugins/config/alpha.lua
+++ lua/user/plugins/config/alpha.lua
@@ -26,16 +26,12 @@ end
 local header = {
     type = "text",
     val = {
-        "              ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                ",
-        "              ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                ",
         " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
         " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
         " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
         " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
         " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
         " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-        "              ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                ",
-        "              ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                ",
     },
     opts = { position = "center", hl = highlight },
 }
@@ -53,7 +49,11 @@ local buttons = {
     val = {
         set_button("1", "  Load Last Session", "<cmd>SessionManager load_last_session<CR>"),
         set_button("2", "  Browse Sessions", "<cmd>SessionManager load_session<CR>"),
-        set_button("3", "  Recent Files", "<cmd>lua require('user.plugins.config.telescope.sources').frecency()<CR>"),
+        set_button(
+            "3",
+            "  Recent Files",
+            "<cmd>lua require('user.plugins.config.telescope.sources').frecency()<CR>"
+        ),
         set_button("4", "  Find Files", "<cmd>Telescope find_files<CR>"),
         set_button("5", "  New File", "<cmd>enew<CR>"),
         set_button("u", "  Update", "<cmd>PackerSync<CR>"),
@@ -64,14 +64,18 @@ local buttons = {
     },
 }
 
+local top_margin_per = 0.3
+local header_padding = vim.fn.max({ 2, vim.fn.floor(vim.fn.winheight(0) * top_margin_per) })
+
 local opts = {
     layout = {
-        { type = "padding", val = 2 },
+        { type = "padding", val = header_padding },
         header,
-        { type = "padding", val = 2 },
+        { type = "padding", val = 4 },
         buttons,
-        { type = "padding", val = 2 },
+        { type = "padding", val = 1 },
         footer,
+        { type = "padding", val = 2 },
     },
     opts = {
         margin = 5,
