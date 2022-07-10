a4dcae387e11aa741299c8c06d3c2a6efe67c7b9
Neel Basak <neelfrost@gmail.com>
2022-07-10 14:45:16 +0530

    feat(cokeline): add separators fix(cokeline): truncation and buffer pick, close mappings

--- lua/user/plugins/config/cokeline.lua
+++ lua/user/plugins/config/cokeline.lua
@@ -8,25 +8,32 @@ local utils = require("user.plugins.config.themer.utils")
 
 local active_fg = colors.blue
 local active_bg = colors.bg.base
-local inactive_fg = utils.adjust_color(colors.fg, -150)
+local inactive_fg = utils.adjust_color(colors.fg, -100)
 local inactive_bg = utils.adjust_color(colors.bg.base, 5)
 local modified_fg = colors.red
 local switch_fg = colors.green
 
 --- Variable padding component
---- @param n number amount of padding (default: 1)
---- @return table component
-local padding = setmetatable({ text = " " }, {
+--- @param n number Amount of padding (default: 1)
+--- @return table Padding component
+local padding = setmetatable({
+    text = " ",
+    truncation = { priority = 1 },
+}, {
     __call = function(_, n)
-        return { text = string.rep(" ", n) }
+        return {
+            text = string.rep(" ", n),
+            truncation = { priority = 1 },
+        }
     end,
 })
 
 --- Poor man's superscript converter
---- @param n number number to be converted into superscript
---- @return string converted superscript number
+--- @param n number Number to be converted into superscript
+--- @return string Converted superscript number
 local function superscript(n)
     local superscripts = { "⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹" }
+
     local str = tostring(n)
     local digits = {}
     for i = 1, #str do
@@ -39,6 +46,10 @@ end
 local is_picking_focus = require("cokeline.mappings").is_picking_focus
 local is_picking_close = require("cokeline.mappings").is_picking_close
 local buffer_ignore_types = { "terminal", "quickfix" }
+
+local function is_picking_focus_or_close()
+    return is_picking_focus() or is_picking_close()
+end
 
 require("cokeline").setup({
     show_if_buffers_are_at_least = 1,
@@ -74,50 +85,65 @@ require("cokeline").setup({
 
     default_hl = {
         fg = function(buffer)
-            return buffer.is_focused and active_fg or inactive_fg
+            return (buffer.is_focused and not (is_picking_focus_or_close())) and active_fg or inactive_fg
         end,
         bg = function(buffer)
-            return buffer.is_focused and active_bg or inactive_bg
+            return (buffer.is_focused and not (is_picking_focus_or_close())) and active_bg or inactive_bg
         end,
         style = function(buffer)
-            return buffer.is_focused and "bold" or nil
+            return (buffer.is_focused and not (is_picking_focus_or_close())) and "bold" or nil
         end,
     },
 
     components = {
+        {
+            text = "|",
+            fg = inactive_fg,
+            bg = inactive_bg,
+            truncation = { priority = 1 },
+        },
         padding(2),
+        {
+            text = function(buffer)
+                return is_picking_focus_or_close() and buffer.pick_letter .. " " or buffer.devicon.icon
+            end,
+            fg = function(buffer)
+                if is_picking_focus() then
+                    return switch_fg
+                elseif is_picking_close() then
+                    return modified_fg
+                elseif buffer.is_focused then
+                    return buffer.devicon.color
+                else
+                    return inactive_fg
+                end
+            end,
+            truncation = { priority = 1 },
+        },
         {
             text = function(buffer)
                 return buffer.unique_prefix
             end,
+            truncation = {
+                priority = 3,
+                direcion = "right",
+            },
         },
         {
             text = function(buffer)
                 return buffer.filename
             end,
             truncation = {
-                priority = 10,
+                priority = 2,
                 direcion = "left",
             },
         },
         padding,
         {
             text = function(buffer)
-                if is_picking_focus() or is_picking_close() then
-                    return buffer.pick_letter
-                end
-
                 return superscript(buffer.index)
             end,
-            fg = function()
-                if is_picking_focus() then
-                    return switch_fg
-                elseif is_picking_close() then
-                    return modified_fg
-                else
-                    return nil
-                end
-            end,
+            truncation = { priority = 1 },
         },
         padding,
         {
@@ -128,8 +154,17 @@ require("cokeline").setup({
             fg = function(buffer)
                 return buffer.is_modified and modified_fg or nil
             end,
+            truncation = { priority = 1 },
         },
         padding(2),
+        {
+            text = function(buffer)
+                return buffer.is_last and "|" or ""
+            end,
+            fg = inactive_fg,
+            bg = inactive_bg,
+            truncation = { priority = 1 },
+        },
     },
 })
 
@@ -140,5 +175,9 @@ map("n", "<Tab>", "<Plug>(cokeline-focus-next)", opts)
 map("n", "<S-Tab>", "<Plug>(cokeline-focus-prev)", opts)
 map("n", "<M-Left>", "<Plug>(cokeline-switch-prev)", opts)
 map("n", "<M-Right>", "<Plug>(cokeline-switch-next)", opts)
-map("n", "<Leader>bf", "<Plug>(cokeline-pick-focus)", opts)
-map("n", "<Leader>bc", "<Plug>(cokeline-pick-close)", opts)
+map("n", "<Leader>bf", function()
+    require("cokeline.mappings").pick("focus")
+end, opts)
+map("n", "<Leader>bc", function()
+    require("cokeline.mappings").pick("close")
+end, opts)
