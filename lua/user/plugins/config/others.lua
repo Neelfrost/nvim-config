624c5dd90ecff658521bb199937bc42269d59afc
Neelfrost <neelfrost@gmail.com>
2024-05-20 22:31:53 +0530

    fix: switch to defaults for lspsignature

--- lua/user/plugins/config/others.lua
+++ lua/user/plugins/config/others.lua
@@ -59,13 +59,11 @@ M.lsp_signature = function()
     require("lsp_signature").setup({
         bind = true,
         hint_enable = false,
-        hint_prefix = "",
-        floating_window = true,
-        hi_parameter = "ThemerHeadingH1",
-        extra_trigger_chars = { "(", "," },
         handler_opts = {
             border = "single",
         },
+        -- hi_parameter = "ThemerHeadingH1",
+        -- extra_trigger_chars = { "(", "," },
     })
 end
 
